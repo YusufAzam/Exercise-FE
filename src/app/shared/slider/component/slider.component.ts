9d4baabb409fd7d27be57478f7926982975577e6
Yusuf Azam <yusufazam@lostcoding.com>
2023-07-25 00:43:59 +0100

    making it look pretty

--- src/app/shared/slider/component/slider.component.ts
+++ src/app/shared/slider/component/slider.component.ts
@@ -1,42 +1,68 @@
-import { IfStmt } from '@angular/compiler';
-import { Component, DoCheck, Input, OnChanges, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
 })
-export class SliderComponent implements DoCheck {
+export class SliderComponent implements OnInit {
 
-  abc: boolean;
   currentSlide: number
+  subscription !: Subscription;
+  source: Observable<number>
+  direction: boolean;
+
   constructor(){ 
-   this.abc = false;
    this.currentSlide = 1;
+   this.source = interval(4000);
+   this.direction = true;
   }
  
-  ngDoCheck(): void {
+  ngOnInit(): void {
+    this.subscription = this.source.subscribe(value => this.autoSlide());
+  }
 
+  private resetTimer(){
+    this.subscription.unsubscribe();
+    this.subscription = this.source.subscribe(value => this.autoSlide());
   }
   
   public plusSlide(){
     console.log(this.currentSlide)
     
-    if(this.currentSlide !== 2){
+    if(this.currentSlide !== 3){
       this.currentSlide ++;
-     
+      this.resetTimer();
     }
   }
 
   public minusSlide(){
     if(this.currentSlide !== 1){
       this.currentSlide --;
+      this.resetTimer();
     }
   }
 
-  // private showSlides = (n: number)=> {
-  //   let i;
-  //   let slides = 
-  // }
+  public autoSlide(){
+      if(this.currentSlide === 3){
+        this.direction = false;
+      }
+
+      if(this.currentSlide === 1){
+        this.direction = true;
+      }
+    
+      if(this.direction){
+          this.currentSlide ++;
+      }else {
+          this.currentSlide --;  
+      }
+  }
+
+  public setCurrentSlide(currentSlide: number){
+    this.currentSlide = currentSlide;
+    this.resetTimer();
+  }
 
 }
